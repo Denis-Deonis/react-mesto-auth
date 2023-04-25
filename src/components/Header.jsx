6f12042c51cda17f658de181553bd2427b84aca2
Denis Zykov <denis.zykov.19932@yandex.ru>
2023-04-25 15:19:50 +0300

    feat: add ProtectedRoute and change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import logo from '../images/header/logo.svg';
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-export default function Header() {
+export default function Header(props) {
   return(
     <header className='header'>
       <img className='header__logo' src={logo} alt='логотип сервиса Mesto' />
+      <nav className="header__auth">
+        <p className="header__text">{props.email}</p>
+        <Link
+          to={props.route}
+          className="header__link link-opacity"
+          type="button"
+          onClick={props.onClick}
+        >
+          {props.title}
+        </Link>
+      </nav>
     </header>
   )
 }
