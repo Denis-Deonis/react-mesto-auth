9c778b59a862df4a201c7487acf4451c82f10470
Denis-Deonis <73986601+Denis-Deonis@users.noreply.github.com>
2023-05-02 16:43:15 +0300

    fixed: return folder public

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 
 export default function Footer(){
 
-  const dt = new Date();
-  const year = dt.getFullYear();
-
-
   return(
     <footer className="footer">
-      <p className="footer__title">&copy; {year} DenisZыkov - Mesto Russia </p>
+      <p className="footer__title">&copy; {new Date().getFullYear()} DenisZыkov - Mesto Russia </p>
     </footer>
   )
 }
