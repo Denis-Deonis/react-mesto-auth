e8b925d1efb96796856dffd6dfac284c56dd9a96
Denis-Deonis <73986601+Denis-Deonis@users.noreply.github.com>
2023-05-04 17:07:08 +0300

    feat: change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,32 @@
 import logo from '../images/header/logo.svg';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HeaderMobileMenu from './HeaderMobileMenu';
 
 export default function Header(props) {
   return(
-    <header className='header'>
-      <img className='header__logo' src={logo} alt='логотип сервиса Mesto' />
-      <nav className="header__auth">
-        <p className="header__text">{props.email}</p>
-        <Link
-          to={props.route}
-          className="header__link"
-          type="button"
-          onClick={props.onClick}
-        >
-          {props.title}
-        </Link>
-      </nav>
-    </header>
+    <>
+      <header className='header header__container'>
+        <a href="foo" target="_self">
+          <img className='header__logo' src={logo} alt='логотип сервиса Mesto' />
+        </a>        
+        <nav className="header__auth">
+          <p className="header__text">{props.email}</p>
+          <Link
+            to={props.route}
+            className="header__link"
+            type="button"
+            onClick={props.onClick}
+          >
+            {props.title}
+          </Link>
+        </nav>
+      </header>
+      <HeaderMobileMenu
+        email={props.email}
+        handleLogout={props.onSignOut}
+        isMobileMenuOpen={props.isMobileMenuOpen}
+      />
+    </>
   )
 }
