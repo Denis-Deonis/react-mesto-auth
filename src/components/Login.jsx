4043aebf50489f598384d69487bdf564ab1b4b5f
Denis-Deonis <73986601+Denis-Deonis@users.noreply.github.com>
2023-05-12 12:52:27 +0300

    fixed: the console is clean

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,55 +1,19 @@
 import React from 'react';
-import { useValidation } from "../hooks/validition.hook";
-import ErrorMessage from './ErrorMessage';
-
+import AuthWithForm from './AuthWithForm';
 
 export default function Login(props) {
 
-  const { values, handleChange, errors, isValid,  } =  useValidation();
-
-  function handleSubmit(evt) {
-    evt.preventDefault()
-    props.onLogin(values.email,  values.password ) 
+  function handleSubmit(email, password) {
+    props.onLogin(email, password) 
   }
 
   return (
-    <section className="auth">
-      <h2 className="auth__title">Вход</h2>
-      <form className="auth__form" onSubmit={handleSubmit}>
-        <input
-          className="auth__input"
-          type="email"
-          placeholder="Email"
-          required
-          value={values.email}
-          onChange={handleChange}
-        />
-        <ErrorMessage message={errors.email} />
-        <input
-          className="auth__input"
-          type="password"
-          placeholder="Пароль"
-          autoComplete="on"
-          minLength="4"
-          required
-          value={values.password}
-          onChange={handleChange}
-        />
-        <ErrorMessage message={errors.password} />
-        <button
-          className={`auth__submit ${
-            !isValid
-              ? 'popup__save_disabled'
-              : ''
-          }`}
-          type="submit"
-          disabled={!isValid}
-        >
-          Войти
-        </button>
-
-      </form>
-    </section>
+    <AuthWithForm 
+    title="Вход"
+    buttonText="Войти"
+    onSubmit={handleSubmit}
+    isSubmitting={props.isSubmitting}
+    />
   )
 
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,65 +1,20 @@
 import React from 'react';
-import { useValidation } from "../hooks/validition.hook";
-import ErrorMessage from './ErrorMessage';
-import { Link } from 'react-router-dom';
+import AuthWithForm from './AuthWithForm';
 
 export default function Register(props) {
 
-  const { values, handleChange, errors, isValid,  } =  useValidation();
-
-  function handleSubmit(evt) {
-    evt.preventDefault()
-    props.onRegister(values.email,  values.password) 
+  function handleSubmit(email, password) {
+    props.onRegister(email, password) 
   }
 
   return (
-    <section className="auth">
-      <h2 className="auth__title">Регистрация</h2>
-      <form className="auth__form" onSubmit={handleSubmit}>
-        <input
-          className="auth__input"
-          type="email"
-          placeholder="Email"
-          required
-          value={values.email}
-          onChange={handleChange}
-        />
-        <ErrorMessage message={errors.email} />
-        <input
-          className="auth__input"
-          type="password"
-          placeholder="Пароль"
-          autoComplete="on"
-          minLength="4"
-          required
-          value={values.password}
-          onChange={handleChange}
-        />
-        <ErrorMessage message={errors.password} />
-        <button
-          className={`auth__submit ${
-            !isValid
-              ? 'popup__save_disabled'
-              : ''
-          }`}
-          type="submit"
-          disabled={!isValid}
-        >
-          Зарегистрироваться
-        </button>
-        <div className="auth__signup">
-          <p className="auth__signup_text">
-            Уже зарегистрированы?{' '}
-          </p>
-          <Link
-            to={"/sign-in"}
-            className="auth__signup_link"
-          >
-            Войти
-          </Link>
-        </div>
-      </form>
-    </section>
+
+    <AuthWithForm
+      title="Регистрация"
+      buttonText="Зарегистрироваться"
+      onSubmit={handleSubmit}
+      isSubmitting={props.isSubmitting}
+    />
   )
 
 
